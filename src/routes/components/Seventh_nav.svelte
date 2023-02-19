9e7e0e5c207aed7945a544bfa7cd3dac57c12fde
kosme <casadadaista@gmail.com>
2023-02-19 12:58:16 -0300

    eighth started

--- src/routes/components/Seventh_nav.svelte
+++ src/routes/components/Seventh_nav.svelte
@@ -4,53 +4,35 @@
 
 <nav>
 	<ul>
-    <div class="left_nav">
-      <a href="/"> <img src="src/assets/dribbble.svg" alt="logo" srcset=""> </a>
-
-    </div>
-		<div class="center_nav">
+		<div class="right_nav">
+			<a href="/"> <img src="src/assets/dribbble.svg" alt="logo" srcset="" /> </a>
 		</div>
-    <div class="right_nav">
-      <img src="src/assets/behance-2.svg" alt="logo" srcset="">
-      <img src="src/assets/mail-1.svg" alt="logo" srcset="">
-      <img src="src/assets/dribbble.svg" alt="logo" srcset="">
-  
-    </div>
 	</ul>
 </nav>
 
 <style lang="scss">
 	nav {
-padding: 2rem 5rem 2rem 5rem;
+		padding: 1rem 2rem 1rem 2rem;
+		position: fixed;
+		width: 100%;
+		top: 0;
+		text-transform: uppercase;
+		font-weight: 700;
 		ul {
 			display: flex;
 			align-items: center;
 			justify-content: space-between;
-      gap: 5rem;
-      .left_nav{
-        flex-basis: 10%;
-        img{
-          width: 3rem;
-          height: 3rem;
-        }
-      }
-      .center_nav{
-        flex-basis:80%;
-        display: flex;
-        justify-content: space-evenly;
-        // text-transform: uppercase;
-        font-weight: 900;
-      }
-      .right_nav{
-        display: flex;
-        justify-content: center;
-        flex-basis: 10%;
-        img{
-          width: 3rem;
-          height: 3rem;
-        }
-        
-      }
+			gap: 5rem;
+		
+			.right_nav {
+				display: flex;
+				justify-content: flex-end;
+				flex:1;
+				img {
+					width: 3rem;
+					height: 3rem;
+				}
+			}
 		}
 	}
 </style>
