cfb596a2220b0abecdc0a61e9429821ff33820e6
kosme <casadadaista@gmail.com>
2023-02-19 17:12:56 -0300

    finalized

---
+++ src/routes/Projects_1/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+
+</script>
+
+<svelte:head>
+	<title>first project section</title>
+	<meta name="description" content="first" />
+</svelte:head>
+
+<img src="src/assets/1.png" alt="image" srcset="" />
+
+
+<style lang="scss">
+
+</style>
